e7380e270ebbbb3da56ff6f946b0f13c3e8143f2
Nadav Cohen <nadavc104@gmail.com>
2023-03-29 15:04:29 +0300

    added CORS

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import { logger, logEvents } from './middlewares/logger.js'
 import { errorHandler } from './middlewares/errorHandler.js'
+import { corsOptions } from './config/corsOptions.js'
+
+
 const app = express()
+
+app.use(express.json())
+
+app.use(cors(corsOptions))
+
+app.use(cookieParser())
+
 app.use(logger)
 
 app.get('/', (req, res) => {
