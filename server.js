2ca05de72967f0da667975088485c6c1bd5972a7
Nadav Cohen <nadavc104@gmail.com>
2023-03-29 12:34:58 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Api is running')
+})
+app.listen(3500, () => console.log('Server is running on port 3500'))
