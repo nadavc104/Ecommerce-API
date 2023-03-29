0883d48af78dc5efb5d91321f0df8bfd45e784d2
Nadav Cohen <nadavc104@gmail.com>
2023-03-29 14:50:13 +0300

    added errorHandler middleware

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { logger, logEvents } from './middlewares/logger.js'
-
+import { errorHandler } from './middlewares/errorHandler.js'
 const app = express()
 app.use(logger)
 
 app.get('/', (req, res) => {
     res.send('Api is running')
 })
+
+app.use(errorHandler)
 app.listen(3500, () => console.log('Server is running on port 3500'))
