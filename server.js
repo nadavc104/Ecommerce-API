8f3f73be272704791f071f87149cd468f8b48a04
Nadav Cohen <nadavc104@gmail.com>
2023-03-30 08:26:00 +0300

    added connection to MongoDB

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
 import cors from 'cors'
+import * as dotenv from 'dotenv'
 import { logger, logEvents } from './middlewares/logger.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 import { corsOptions } from './config/corsOptions.js'
+import connectDB from './config/connectDB.js'
 
-
+dotenv.config()
 const app = express()
+
+connectDB()
 
 app.use(express.json())
 
 app.use(cors(corsOptions))
-
 app.use(cookieParser())
-
 app.use(logger)
 
 app.get('/', (req, res) => {
@@ -21,4 +24,13 @@ app.get('/', (req, res) => {
 })
 
 app.use(errorHandler)
-app.listen(3500, () => console.log('Server is running on port 3500'))
+
+mongoose.connection.once('open', () => {
+    console.log('connected to mongodb')
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongodbErrLog.log')
+})
